61729291405f25bce32efb28c3522f19ac9f65ad
Alex J <jones6211@gmail.com>
2018-11-09 22:00:22 -0600

    fixing responsivness

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 const inputs = document.querySelectorAll('.controls input');
 
 
-function handleUpdate() {
+function handleUpdate(e) {
     
     const suffix = this.dataset.sizing || '';
     document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix)
+    e.target.nextElementSibling.innerHTML = this.value + suffix;
 }
 
 inputs.forEach(input => input.addEventListener('change', handleUpdate));
 inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+
 
 function inputImage() {
     let inputValue;
