1a8a128aad2c2e843c7de0cf25a19b6c0d63f6cc
Alex J <jones6211@gmail.com>
2018-11-10 20:16:04 -0600

    added background color changing feature improved design

--- main.js
+++ main.js
@@ -13,11 +13,23 @@ inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
 
 
 function inputImage() {
-    let inputValue;
 
-    inputValue = document.getElementById("imageInput").value;
+    let inputValue = document.getElementById("imageInput").value;
     
     document.getElementById("mainImg").setAttribute("src", inputValue)
     
+    
+}
+
+function toggleMode() {
+    
+    const color = ["bg-dark", "bg-primary","bg-light", "bg-danger", "bg-sucess", "bg-secondary", "bg-info"];
+    const bodyElement = document.getElementById('body');
+    const toggleBtn = document.getElementById('toggle');
+
+    const currentClass = color.filter(item => {if(item == bodyElement.classList) return item } );
+    const colorIndex = color.indexOf(currentClass[0]);
+    bodyElement.classList == color[colorIndex] ? bodyElement.classList.replace(color[colorIndex], color[colorIndex + 1]) : bodyElement.classList.replace(undefined, color[0]);
+
     
 }
